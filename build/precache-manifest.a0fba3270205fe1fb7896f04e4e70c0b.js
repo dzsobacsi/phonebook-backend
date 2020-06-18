cf7e5e3012a234c43126a9c0558b0bc71aee7893
dzsobacsi <tberki77@gmail.com>
2020-06-18 07:54:49 +0200

    frontend added

---
+++ build/precache-manifest.a0fba3270205fe1fb7896f04e4e70c0b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1699bd53123cd5671d264d4b842105fd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "58bd7345623f9bd76703",
+    "url": "/static/js/2.e3d3a486.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e3d3a486.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5c24b5e3715e8f3b740",
+    "url": "/static/js/main.8291348f.chunk.js"
+  },
+  {
+    "revision": "37e42bc47805ba865915",
+    "url": "/static/js/runtime-main.fac6ad05.js"
+  }
+]);
